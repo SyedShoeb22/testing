3f9e4a67ed611b599390454fdb26151b99d0c232
SyedShoeb22 <syedshoeb8380@gmail.com>
2025-06-17 16:45:01 +0530

    create wrangler.toml

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith('/static/')) {
-      url.hostname = 'pub-20771ffbfa2343be8b01ca6bf4b7046c.r2.dev';  // Replace with your R2 public URL
-      url.pathname = '/lxp-static' + url.pathname;
-      return fetch(url.toString());
+    if (url.pathname.startsWith("/static/")) {
+      const originUrl = `https://pub-20771ffbfa2343be8b01ca6bf4b7046c.r2.dev/lxp-static${url.pathname}`;
+      console.log("Proxying to:", originUrl);
+      return fetch(originUrl);
     }
 
-    return new Response('Not Found', { status: 404 });
+    return new Response("Not Found", { status: 404 });
   },
 };
