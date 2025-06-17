49586eb7d3ec72596d4b9da298d20ded11797bb8
SyedShoeb22 <syedshoeb8380@gmail.com>
2025-06-17 17:38:43 +0530

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
+    const url = new URL(request.url)
 
-    if (url.pathname.startsWith("/static/")) {
-      const originUrl = `https://pub-20771ffbfa2343be8b01ca6bf4b7046c.r2.dev/lxp-static${url.pathname}`;
-      console.log("Proxying to:", originUrl);
-      return fetch(originUrl);
+    if (url.pathname.startsWith('/static/')) {
+      // Remove '/static' and append the actual path under 'lxp-static'
+      const rewrittenPath = url.pathname.replace('/static', '')
+      const r2Url = `https://pub-20771ffbfa2343be8b01ca6bf4b7046c.r2.dev/lxp-static${rewrittenPath}`
+      return fetch(r2Url)
     }
 
-    return new Response("Not Found", { status: 404 });
-  },
-};
+    return new Response('Not Found', { status: 404 })
+  }
+}
